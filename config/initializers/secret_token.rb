8ca100d37090b02235e0f852d672f5e7d893650d
sheyduh <sheyxpeare@gmail.com>
2014-01-31 17:08:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaeedi::Application.config.secret_key_base = '918b66d5448f8edfd13d29324492b24d6b5188297739dae7ac63aa140d4c31878c64d2e8275b4775c289da97d9bcbaa6d04fa93127342a1ebf5b9d086c58fda4'
